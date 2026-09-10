f7a0f576f47b057dbf0a6888ee261f82e7226b10
RafaMadera18 <rafa_madlop@hotmail.com>
2026-09-10 11:43:22 -0600

    Implementacion B

--- src/customer.py
+++ src/customer.py
@@ -11,6 +11,8 @@ class Customer:
 
 
 def update_customer_email(customer, new_email, updated_by):
-    customer.email = new_email
+    if "@" not in new_email:
+        raise ValueError("invalid-email")
+    customer.email = new_email.lower()
     customer.updated_by = updated_by
     return customer
